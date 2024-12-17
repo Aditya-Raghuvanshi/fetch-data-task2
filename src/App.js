d3af3ae80a631b43b3088e83f09df95fd1358df0
Aditya Raghuvanshi <adityaraghuvanshi159@gmail.com>
2024-12-17 10:22:21 +0530

    added a change

--- src/App.js
+++ src/App.js
@@ -5,11 +5,33 @@ import Navbar from './components/Navbar';
 import store from './utils/store';
 import { clarity } from 'react-microsoft-clarity';
 import { useEffect } from 'react';
+// import ReactGA from 'react-ga4'
 
 function App() {
+  // useEffect(() => {
+  //   // Initialize Google Analytics with your GA4 Measurement ID
+  //   ReactGA.initialize('G-XXXXXXX');  // Replace with your Google Analytics 4 ID
+  // }, []);
   useEffect(() => {
     // Initialize Clarity with your project ID
     clarity.init('peqksstinr');
+    const identifyUser = (userId, userName, userEmail) => {
+      // Identifying the user with their unique ID and additional info
+      clarity.identify(userId, {
+        name: userName,
+        email: userEmail,
+      });
+    };
+    const user = {
+      id: 'd80zb9',
+      name: 'aditya',
+      email: 'aditya@yopmail.com'
+      };
+      
+    identifyUser(user?.id, user?.name, user?.email);
+    clarity.setTag('set','id',user?.id)
+    clarity.setTag('set','name',user?.name)
+    clarity.setTag('set','email',user?.email)
   }, []);
 
   return (
